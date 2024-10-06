f1d21c6f13b0f3b71076a270b40cf62b8fd8ee00
AmerDakelbab <142162220+AmerDakelbab@users.noreply.github.com>
2024-10-06 18:57:09 +0300

    first commit

--- frontend/src/components/CategoryProducts.jsx
+++ frontend/src/components/CategoryProducts.jsx
@@ -28,13 +28,18 @@ function CategoryProducts({isVisible,addToCart}) {
 
 
   return (
-    <div className='bg-gray-200 h-screen w-screen'>
-<div className={`bg-gray-200 h-fit w-screen grid xs: grid-cols-2  sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 z-10 transition-all duration-300 ${isVisible ? 'ml-32 sm:ml-64 md:ml-64 lg:ml-64 xl:ml-64 2xl:ml-64' : 'ml-1'}`}>
-  {products.map((product, index) => (
-        <div key={index} className='flex flex-col ml-2 mt-20 p-4 bg-white w-48 justify-center items-center' >
+    <div className='bg-gray-200 h-screen w-screen overflow-hidden'>
+  <div className={`bg-gray-200 h-full w-screen grid xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 z-10 transition-all duration-300 ${isVisible ? 'ml-32 sm:ml-64 md:ml-64 lg:ml-64 xl:ml-64 2xl:ml-64' : 'ml-1'}`}>
+    
+    {products.length === 0 ? (
+      <div className="col-span-full h-full flex items-center justify-center">
+        <p className='text-3xl text-center'>No products available in this category.</p>
+      </div>
+    ) : (
+      products.map((product, index) => (
+        <div key={index} className='flex flex-col ml-2 mt-20 p-4 bg-white w-48 justify-center items-center'>
           <div>
-          <img className='h-60 w-60 object-fit' src={`http://localhost:5000/${product.image}`} alt={product.product_name} /> 
-
+            <img className='h-60 w-60 object-fit' src={`http://localhost:5000/${product.image}`} alt={product.product_name} />
             <div className='flex'>
               <div className='font-bold'>{product.category}</div> /
               <div className='font-bold'>{product.product_name}</div>
@@ -44,9 +49,12 @@ function CategoryProducts({isVisible,addToCart}) {
             <div className='font-bold'>{product.stock_status}</div>
             <ShoppingCartOutlined onClick={() => addToCart(product)} className='cursor-pointer' />
           </div>
-        </div>))}
         </div>
-        </div>
+      ))
+    )}
+  </div>
+</div>
+
   );
 }
 
